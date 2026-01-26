2f0513eb0dfdae160014756786d03f9f0bc707b1
Ravi Teja Geddada <51739120+Raviteja77@users.noreply.github.com>
2026-01-25 21:45:17 -0600

    Create admin dashboard layout

---
+++ services/web/src/app/(admin)/admin/layout.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+export default function AdminDashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col gap-6">
+      {children}
+    </div>
+  );
+}
